ae8f89a036d8fac9a22a092d14abb20347dad54c
tsharpe <tsharpe@broadinstitute.org>
2015-03-20 14:12:34 -0400

    Removed outdated headers that referred to Apache license, added new LICENSE.txt.  Removed server names from README.md and src/test/resources/boss-config.yml.

--- boss-ws/src/test/java/org/genomebridge/boss/http/BossDAOTransactionTest.java
+++ boss-ws/src/test/java/org/genomebridge/boss/http/BossDAOTransactionTest.java
@@ -1,12 +1,3 @@
-/*
- * The Broad Institute
- * SOFTWARE COPYRIGHT NOTICE AGREEMENT
- * This software and its documentation are copyright 2015 by the
- * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
- *
- * This software is supplied without any warranty or guaranteed support whatsoever. Neither
- * the Broad Institute nor MIT can be responsible for its use, misuse, or functionality.
- */
 package org.genomebridge.boss.http;
 
 import io.dropwizard.testing.junit.DropwizardAppRule;
